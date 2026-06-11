0dd0ce184e8b90914d702d0a65a41613cb34708a
practicalusage <rt@practicalusage.com>
2026-06-11 14:04:09 -0400

    Update clientInfo.h

--- Production Code/clientInfo.h
+++ Production Code/clientInfo.h
@@ -1,4 +1,4 @@
-const char* ssid = "rtls";                 // your network SSID (name of wifi network)
-const char* password = "leborddulac";      // your network password
-const char* mqtt_server = "192.168.2.230";  // your mqtt server ip
+const char* ssid = "YOUR SSID";                 // your network SSID (name of wifi network)
+const char* password = "YOUR WIFI PASSWORD";      // your network password
+const char* mqtt_server = "MQTT SERVE IP ADDRESS";  // your mqtt server ip
 const int mqtt_port = 1883;                // your mqtt server port
